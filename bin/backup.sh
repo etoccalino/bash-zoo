ec3ab5bfca8312139fc1e0e9623327c1a3f2e069
Elvio Toccalino <me@etoccalino.com>
2013-11-04 00:41:14 -0300

    started the backup script docs

---
+++ bin/backup.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+#
+# backup.sh
+#
+# Depends on:
+#   * obnam
+#   * gpg-agent or equivalent (see obnam docs)
+#
+# Assumes:
+#   * The ~/.obnam.conf file has been installed and edited
+#   * There's a valid GPG key configured in ~/.obnam.conf
+#
