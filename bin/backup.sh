077db02f3c8503f2a90b3f29aff1431f6cec052d
Elvio Toccalino <me@etoccalino.com>
2013-11-04 01:56:47 -0300

    backup done

--- bin/backup.sh
+++ bin/backup.sh
@@ -11,3 +11,19 @@
 #   * The ~/.obnam.conf file has been installed and edited
 #   * There's a valid GPG key configured in ~/.obnam.conf
 #
+
+TO_BACKUP="$HOME/Documents $HOME/Pictures"
+
+obnam backup ${TO_BACKUP}
+
+#
+# To restore the backups
+#
+#  obname restore --to=/tmp/backup ${TO_BACKUP}
+#
+# To forget the backups
+#
+#   obnam forget 4w  # to keep latest backup of the week, for a month
+#   or
+#   obnam forget 1m  # keep only the latest backup of the last month
+#
