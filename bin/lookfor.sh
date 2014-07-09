12ca7f5bf27effe7c0f18b492c37b7c5e97818e0
Elvio Toccalino <me@etoccalino.com>
2014-07-09 15:40:22 -0300

    allow lookfor.sh to accept multiple words

--- bin/lookfor.sh
+++ bin/lookfor.sh
@@ -5,14 +5,14 @@ SEP="----------------------------"
 echo "${SEP} file names ${SEP}"
 find . -iname "*${1}*"
 echo "${SEP} txt ${SEP}"
-find . -name '*.txt' -print0 | xargs -0 grep -i --color --with-filename -n $@
+find . -name '*.txt' -print0 | xargs -0 grep -i --color --with-filename -n "$@"
 echo "${SEP} html ${SEP}"
-find . -name '*.html' -print0 | xargs -0 grep -i --color --with-filename -n $@
+find . -name '*.html' -print0 | xargs -0 grep -i --color --with-filename -n "$@"
 echo "${SEP} js ${SEP}"
-find . -name '*.js' -print0 | xargs -0 grep -i --color --with-filename -n $@
+find . -name '*.js' -print0 | xargs -0 grep -i --color --with-filename -n "$@"
 echo "${SEP} php ${SEP}"
-find . -name '*.php' -print0 | xargs -0 grep -i --color --with-filename -n $@
+find . -name '*.php' -print0 | xargs -0 grep -i --color --with-filename -n "$@"
 echo "${SEP} rb ${SEP}"
-find . -name '*.rb' -print0 | xargs -0 grep -i --color --with-filename -n $@
+find . -name '*.rb' -print0 | xargs -0 grep -i --color --with-filename -n "$@"
 echo "${SEP} py ${SEP}"
-find . -name '*.py' -print0 | xargs -0 grep -i --color --with-filename -n $@
+find . -name '*.py' -print0 | xargs -0 grep -i --color --with-filename -n "$@"
